9cf9ca4d6fca78348e8f6f5a0a9d3e854a7a259e
samiullahchchch <sami03136424477@gmail.com>
2024-11-24 23:57:40 +0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import { getPostData } from '../lib/posts';
+
+export default function Home({ postData }) {
+    return (
+        <div>
+            <h1>{postData.data.title}</h1>
+            <p>{postData.data.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: postData.content }} />
+        </div>
+    );
+}
+
+export async function getStaticProps() {
+    const postData = getPostData();
+    return {
+        props: {
+            postData,
+        },
+    };
+}
